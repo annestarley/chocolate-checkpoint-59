1ab886f6ec11c6cbb664e60c34ec878d84819b1d
Anne Starley <annestarley@gmail.com>
2018-01-18 11:39:29 -0800

    added app.js, model.js, and controller.js. Edited app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 6001
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const uuid = require('uuid/v4')
+
+const controller = require('./controller')
+const model = require('./model')
+
+app.disable('x-powered-by')
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+
+
+
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  res.status(status).json({ error: err })
+})
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: { message: 'Not found' }})
+})
+
+const listener = () => `Listening on port ${port}!`
+app.listen(port, listener)
+
+module.exports = app
