bbdc15d473d6b4da53c7af97f560fa3e6d939b4f
Anne Starley <annestarley@gmail.com>
2018-01-18 13:28:39 -0800

    able to view all, view individual, and create chocolates

--- controller.js
+++ controller.js
@@ -1,15 +1,22 @@
 const model = require('./model')
 
 const chocolatesController = (req, res, next) => {
-
+  const chocolates = model.getAllChocolates()
+  res.json(chocolates)
 }
 
 const chocolateByIdController = (req, res, next) => {
-
+  const id = req.params.id
+  const chocolate = model.getChocolateById(id)
+  if (!chocolate) return next({ status: 404, message: `Could not find chcolate with id of ${id}.` })
+  res.json(chocolate)
 }
 
 const chocolateCreaterController = (req, res, next) => {
-
+  const { name, yumminess } = req.body
+  if (!name || !yumminess) return next({ status: 400, message: `Required: chocolate name and chocolate yumminess(out of 10).`})
+  const chocolate = model.createChocolate(name, yumminess)
+  res.status(201).json(chocolate)
 }
 
 const chocholateUpdaterController = (req, res, next) => {
--- model.js
+++ model.js
@@ -1,24 +1,60 @@
-const uuid = require('uuid/v4')
+const uuid = require('uuid')
 
 const chocolates = [
   {
-    id: "something",
+    id: "5a4788d7-5490-4737-8ece-8e1862e24957",
     name: "fudge",
     yumminess: "10/10"
   },
   {
-    id: "somethingelse",
+    id: "08e0eda1-5254-4145-aa30-9e57d3c0c52f",
     name: "truffles",
     yumminess: "10/10"
   },
   {
-    id: "someotherthing",
+    id: "43446794-8d09-4758-a9a8-0b7bc411dac8",
     name: "hot chocolate",
     yumminess: "2000000/10"
   },
   {
-    id: "anewthing",
+    id: "3eb511dc-2146-4cd2-855b-d8c098371dee",
     name: "milk choclate with caramel",
     yumminess: "10/10"
+  },
+  {
+    "id": "12f5b7f1-d789-4d8f-87eb-ae05d3518fa5",
+    "name": "reeses",
+    "yumminess": "10/10"
   }
 ]
+
+function getAllChocolates() {
+  return chocolates
+}
+
+function getChocolateById(id) {
+  let chocolate = chocolates.find(chocolate => chocolate.id === id)
+  if (chocolate) return chocolate
+}
+
+function createChocolate(name, yumminess) {
+  const chocolate = { id: uuid(), name, yumminess}
+  chocolates.push(chocolate)
+  return chocolate
+}
+
+function updateChocolate() {
+
+}
+
+function deleteChocolate() {
+
+}
+
+module.exports = {
+  getAllChocolates,
+  getChocolateById,
+  createChocolate,
+  updateChocolate,
+  deleteChocolate
+}
